de72c97ac8ffda6559adc86b7e860ece6650e228
sonidef <sonja.elarco@gmail.com>
2020-02-10 15:12:40 -0700

    added assignment files

---
+++ code/app.js
@@ -0,0 +1,63 @@
+const green = 'rgba(118,174, 170, 1)'
+const black = 'rgba(23,35, 50, 1)'
+const brown = 'rgba(205,164, 133, 1)'
+const greenblue = 'rgba(62,171, 201, 1)'
+const pink = 'rgba(254,156, 161, 1)'
+const blue = 'rgba(98,189, 254, 1)'
+
+const data = [
+    {
+        topic: 'Food',
+        title: 'Wake Up and Smell the Coffee',
+        price: '$0.90',
+        color: green
+    },
+    {
+        topic: 'Architecture',
+        title: 'The Brand New NASA Office',
+        price: '$0.19',
+        color: black
+    },
+    {
+        topic: 'Travel',
+        title: 'Experience the Saharan Sands',
+        price: '$2.29',
+        color: brown
+    },
+    {
+        topic: 'Interior',
+        title: '9 Air-Cleaning Plants Your Home Needs',
+        price: '$0.09',
+        color: greenblue
+    },
+    {
+        topic: 'Food',
+        title: 'One Month Sugar Detox',
+        price: '$0.99',
+        color: pink
+    },
+    {
+        topic: 'Photography',
+        title: 'Shooting Minimal Instagram Photos',
+        price: '$0.29',
+        color: blue
+    }
+]
+
+const cardText = document.querySelector('#card-container')
+
+let html = ""
+
+for (dataProps of data) {
+    html = html + `
+    <div class="cards" style="background-color:${dataProps.color}">
+    <div class="text">
+        <h3>${dataProps.topic}</h3>
+        <h4>${dataProps.title}</h4>
+    </div>
+    <button>Read for ${dataProps.price}</button>
+</div>
+    `  
+}
+
+cardText.innerHTML = html
